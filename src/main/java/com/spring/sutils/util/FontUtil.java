a5e1abd3ce5f20dce709fa54db266467e86e2231
huiqiang <huiqiangjia@huangbaoche.com>
2018-08-01 20:22:02 +0800

    自定义加载字体

--- src/main/java/com/spring/sutils/util/FontUtil.java
+++ src/main/java/com/spring/sutils/util/FontUtil.java
@@ -15,6 +15,22 @@ import java.io.IOException;
  */
 public class FontUtil {
 
+
+    /*
+
+
+    Class.getResource(String path)
+path不以’/'开头时，默认是从此类所在的包下取资源；
+path  以’/'开头时，则是从ClassPath根下获取；
+
+Class.getClassLoader（）.getResource(String path)
+path不能以’/'开头时；
+path是从ClassPath根下获取；
+
+Class.getClassLoader（）.getResource和Class.getClassLoader（）.getResourceAsStream在使用时，路径选择上也是一样的。
+
+TestMain.class.getResource("/") == t.getClass().getClassLoader().getResource("")
+     */
     private  static Logger logger = LoggerFactory.getLogger(WeiXinImgUtil.class);
 
     public static Font getFont(int fontFormat,float size) {
