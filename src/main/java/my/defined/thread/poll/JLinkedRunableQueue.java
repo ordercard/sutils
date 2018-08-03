3c89796b5ba41457b3b88d4d4c3f7a2dd8d0f5c2
huiqiang <huiqiangjia@huangbaoche.com>
2018-08-03 09:51:27 +0800

    自定义实现线程池

--- src/main/java/my/defined/thread/poll/JLinkedRunableQueue.java
+++ src/main/java/my/defined/thread/poll/JLinkedRunableQueue.java
@@ -10,15 +10,15 @@ import java.util.LinkedList;
  * @Date: Create in 下午5:28 2018/8/2 2018
  * @Modify:
  */
-public class JLinkedRunableQueue implements  JRunableQueue {
+public class JLinkedRunableQueue implements JRunableQueue {
     //最大容量
-    private final  int limit;
+    private final int limit;
     //拒绝侧罗  如果任务队列中的任务已经满了，则需要执行拒绝策略
     private final JdencyPolicy jdencyPolicy;
 
-    private final LinkedList<Runnable>  runnableLinkedList =new LinkedList<>();
+    private final LinkedList<Runnable> runnableLinkedList = new LinkedList<>();
 
-    private  final  JThreadPool threadPool;
+    private final JThreadPool threadPool;
 
     public JLinkedRunableQueue(int limit, JdencyPolicy jdencyPolicy, JThreadPool threadPool) {
         this.limit = limit;
@@ -28,39 +28,37 @@ public class JLinkedRunableQueue implements  JRunableQueue {
 
     @Override
     public void offer(Runnable runnable) {
-        synchronized (runnableLinkedList){
-            System.out.println("进入同步加入方法");
+        synchronized (runnableLinkedList) {
 
-       if (runnableLinkedList.size()>=limit){
-           jdencyPolicy.reject(runnable,threadPool);
-       }
-       else{
-           runnableLinkedList.addLast(runnable);
-           runnableLinkedList.notifyAll();
-
-       }}
+            if (runnableLinkedList.size() >= limit) {
+                jdencyPolicy.reject(runnable, threadPool);
+            } else {
+                runnableLinkedList.addLast(runnable);
+                runnableLinkedList.notifyAll();
+                System.out.println(Thread.currentThread().getName() + "进入同步加入方法");//main进入同步方法
+            }
+        }
     }
 
 
     @Override
     public Runnable take() throws InterruptedException {
         synchronized (runnableLinkedList) {
-          while(runnableLinkedList.isEmpty()){
-            try {
-                System.out.println("size为"+runnableLinkedList.size());
-                //如果任务队列中没有可以执行的任务，则当前线程将会挂起，进入runablelist关联的一个对象监视器monitor waitset中等待被唤醒， 这种不和io阻塞和没获取到锁一样，这种可以响应中断并且，被唤醒的方式也不一样
-                System.out.println(Thread.currentThread().getName()+"正在等待！");
-                runnableLinkedList.wait();
-            } catch (InterruptedException e) {
-                System.out.println("抛出异常");
-                throw e;
+            while (runnableLinkedList.isEmpty()) {
+                try {
+                    System.out.println("size为" + runnableLinkedList.size());
+                    //如果任务队列中没有可以执行的任务，则当前线程将会挂起，进入runablelist关联的一个对象监视器monitor waitset中等待被唤醒， 这种不和io阻塞和没获取到锁一样，这种可以响应中断并且，被唤醒的方式也不一样
+                    System.out.println(Thread.currentThread().getName() + "正在等待！");
+                    runnableLinkedList.wait();
+                } catch (InterruptedException e) {
+                    System.out.println("抛出异常");
+                    throw e;
+                }
             }
-        }
             return runnableLinkedList.removeFirst();
         }
 
     }
-
 
 
     @Override
--- src/main/java/my/defined/thread/poll/JThreadTest.java
+++ src/main/java/my/defined/thread/poll/JThreadTest.java
@@ -14,34 +14,33 @@ public class JThreadTest {
     public static void main(String[] args) throws InterruptedException {
 
         //初始值  核心   最大   队列大小
-       final   JThreadPool  jThreadPool = new JBasicThreadPool(2,10,7,1000);
+        final JThreadPool jThreadPool = new JBasicThreadPool(2, 10, 7, 1000);
 
-            for (int i=0;i<20;i++){
-                System.out.println("开始创建"+i);
-                jThreadPool.execute(()->{
-                    try {
-                        TimeUnit.SECONDS.sleep(10);
-                        System.out.println(Thread.currentThread().getName()+"： 正在执行和完成");
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
-                });
-
-                for(;;){
-
+        for (int i = 0; i < 20; i++) {
+            System.out.println("开始创建" + i);
+            jThreadPool.execute(() -> {
+                try {
                     TimeUnit.SECONDS.sleep(10);
-                    System.out.println("活动线程数"+jThreadPool.getActiveCount());
-                    System.out.println("任务池的任务数"+jThreadPool.getQueueSize());
-                    System.out.println("活动最大数core"+jThreadPool.getCoreSize());
-                    System.out.println("活动最大数max"+jThreadPool.getMaxSize());
-                    System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-
-
+                    System.out.println(Thread.currentThread().getName() + "： 正在执行和完成");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
 
+            });
 
-            }
+        }
+
+        for (; ; ) {
+
+            System.out.println("活动线程数" + jThreadPool.getActiveCount());
+            System.out.println("任务池的任务数" + jThreadPool.getQueueSize());
+            System.out.println("活动最大数core" + jThreadPool.getCoreSize());
+            System.out.println("活动最大数max" + jThreadPool.getMaxSize());
+            System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+
+            TimeUnit.SECONDS.sleep(5);
+        }
 
     }
 }
