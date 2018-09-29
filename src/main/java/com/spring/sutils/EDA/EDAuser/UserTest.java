32248c98599c4b05fe6e2695441082dd19fbed2b
huiqiang <huiqiangjia@huangbaoche.com>
2018-09-29 16:21:34 +0800

    EDA框架  EDA框架使用demo    总结

--- src/main/java/com/spring/sutils/EDA/EDAuser/UserTest.java
+++ src/main/java/com/spring/sutils/EDA/EDAuser/UserTest.java
@@ -19,5 +19,11 @@ public class UserTest {
         new UserThread(new User(" LI "),asyncEventDispatcher).start();
         new UserThread(new User(" HUIQIANG "),asyncEventDispatcher).start();
     }
+/*
 
+不用进行同步的原因是 我们的event时间对象被设计成不可变的对象，因为event在经过每一个channel的时间都会创建一个全新的event ，多个线程之间不会出现资源竞争，因此不需要进行同步的保护。 联想生产者消费者模式
+如果使用 EDA框架进行修改。就不需要多个线程之间的交互，也不需要多个线程之间进行同步。有消息提交，消费的handler自然而然的就会触发消费Message。
+end
+
+ */
 }
